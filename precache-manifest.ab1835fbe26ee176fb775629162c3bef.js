1a371d23927e26698ada4aab564344bd7bac50dc
Tom Ocheltree <tom.ocheltree@gmail.com>
2019-02-06 21:26:11 -0600

    Updates

---
+++ precache-manifest.ab1835fbe26ee176fb775629162c3bef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "8b5cf6f92a307e4d8f00",
+    "url": "/clicky-game/static/js/main.8b5cf6f9.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/clicky-game/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "8b5cf6f92a307e4d8f00",
+    "url": "/clicky-game/static/css/main.55e35cc8.chunk.css"
+  },
+  {
+    "revision": "6e59688af54232fe6c82892974617350",
+    "url": "/clicky-game/index.html"
+  }
+];
